ed59722f5c484c7924704e8e8fdceb7e05bfd295
PierreLeboeuf <suckstone@gmail.com>
2025-05-28 16:47:20 +0200

    ajout bass

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+
 // // ---------------------------------------------
-// Insertions des boutons dans le html
+// Insertions des boutons dans le html XYLO
 // // ---------------------------------------------
 
-const body = document.body;
+// const body = document.body;
+let xyloClass = document.querySelector(".xylo");
 // Tableau des notes
 let tableauNote = ["A1", "B1", "C1", "D1", "E1", "F1", "G1", "A2", "B2", "C2", "D2", "E2", "F2", "G2"];
 
@@ -15,13 +18,13 @@ const keyboardNoteMap = {
     "t": "E1",
     "y": "F1",
     "u": "G1",
-    "q": "A1",
-    "s": "B1",
-    "d": "C1",
-    "f": "D1",
-    "g": "E1",
-    "h": "F1",
-    "j": "G1"
+    "q": "A2",
+    "s": "B2",
+    "d": "C2",
+    "f": "D2",
+    "g": "E2",
+    "h": "F2",
+    "j": "G2"
 };
 
 
@@ -31,13 +34,14 @@ for (let note of tableauNote) {
     buttonNote.addEventListener("click", function (event) {
         console.log(event.target.textContent);
         new Audio(`Mallet Note/Mallet ${note}.wav`).play()
+        
     })
     // Création de la classe avec la première lettre de la note
     let lettre = note.split("");
     console.log(lettre[0]);
     buttonNote.classList.add(`${lettre[0]}`)
 
-    body.append(buttonNote); // Inserer les boutons dans le body
+    xyloClass.append(buttonNote); // Inserer les boutons dans le body
 }
 
 document.addEventListener("keydown", function (e) {
@@ -50,3 +54,67 @@ document.addEventListener("keydown", function (e) {
         new Audio(`Mallet Note/Mallet ${noteToPlay}.wav`).play();
     }
 });
+
+// // ---------------------------------------------
+// Insertions des boutons dans le html BASS
+// // ---------------------------------------------
+
+
+let bassClass = document.querySelector(".bass");
+// Tableau des notes
+let tableauNoteBass = ["A", "B", "C", "D", "E", "F", "G", "A#", "C#", "D#", "E2", "F#", "G#"];
+
+// Mapper les notes à des touches spécifiques du clavier
+const keyboardNoteMapBass = {
+    "a": "A",
+    "z": "B",
+    "e": "C",
+    "r": "D",
+    "t": "E",
+    "y": "F",
+    "u": "G",
+    "q": "A#",
+    "d": "C#",
+    "f": "D#",
+    "g": "E2",
+    "h": "F#",
+    "j": "G#"
+};
+
+
+for (let noteBass of tableauNoteBass) {
+    let buttonNoteBass = document.createElement("button");
+    buttonNoteBass.textContent = noteBass;
+    buttonNoteBass.addEventListener("click", function (event) {
+        console.log(event.target.textContent);
+        new Audio(`Bass Note/${noteBass}.wav`).play()
+        
+    })
+    // Création de la classe avec la première lettre de la note
+    let lettreBass = noteBass.split("");
+    console.log(lettreBass[0]);
+    buttonNoteBass.classList.add(`${lettreBass[0]}`)
+
+    bassClass.append(buttonNoteBass); // Inserer les boutons dans le body
+}
+
+document.addEventListener("keydown", function (e) {
+    const pressedKeyBass = e.key.toLowerCase(); // Convertir la touche pressée en minuscule pour la correspondance
+
+    // Vérifier si la touche pressée est dans notre mapping
+    if (keyboardNoteMapBass[pressedKeyBass]) {
+        const noteToPlayBass = keyboardNoteMapBass[pressedKeyBass];
+        console.log(`Touche pressée : ${pressedKeyBass}, Joue : ${noteToPlayBass}`);
+        new Audio(`Mallet Note/Mallet ${noteToPlayBass}.wav`).play();
+    }
+});
+
+// // ---------------------------------------------
+// ajouter ou supprimer une Div
+// // ---------------------------------------------
+
+let xylo = document.querySelector("#xylo");
+xylo.addEventListener("click", function () {
+    xyloClass.classList.toggle("cachee");
+
+  })
